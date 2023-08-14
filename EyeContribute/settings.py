f79162b3b019ab20948f47af8a9adf14edffffe9
DaveTrev <trevaskisdavid@gmail.com>
2023-08-14 16:25:44 +0000

    account registration created

--- EyeContribute/settings.py
+++ EyeContribute/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-davetrev-eyecontribute-mo73833cy33.ws-eu102.gitpod.io',
+ALLOWED_HOSTS = ['8000-davetrev-eyecontribute-mo73833cy33.ws-eu103.gitpod.io',
                  "ecblog.herokuapp.com", "ecblog-2abe5f5bb08f.herokuapp.com"]
 
 
@@ -44,12 +44,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'ecblog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
